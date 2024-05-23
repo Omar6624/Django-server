cb9f418c78fbce9f0790dbe9d69f309ad3197a52
Omar6624 <86346592+Omar6624@users.noreply.github.com>
2024-05-23 12:01:33 +0600

    this is an experiment branch

--- mysite/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import DataShow
-
-class DataShowSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=DataShow
-        fields = '__all__'
